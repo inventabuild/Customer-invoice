a18c5eae396325a732ce080c3d1eaef61c8631a4
inventabuild <mrabushka@hotmail.com>
2022-10-14 20:26:40 -0500

    installed new version of python to try to fix error message

--- invoice_creator/settings.py
+++ invoice_creator/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+#Added from YT video
+import dj_database_url
+from dotenv import load_dotenv, find_dotenv
 
 from pathlib import Path
 import django_on_heroku
@@ -127,8 +130,8 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
+STATIC_URL = '/static/'
+#STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
