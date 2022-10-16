1ed93853bea725fd398cbe8310c82654bdd2c886
inventabuild <mrabushka@hotmail.com>
2022-10-16 00:34:04 -0500

    changing secret key

--- invoice_creator/asgi.py
+++ invoice_creator/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoice_creator.settings.dev')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoice_creator.settings.dev')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
 
 application = get_asgi_application()
